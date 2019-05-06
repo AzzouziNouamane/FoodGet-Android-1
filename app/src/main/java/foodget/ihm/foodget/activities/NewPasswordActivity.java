6828bf8c059be71ed21fef3cb3973a04626fdf56
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2019-05-06 16:42:48 +0200

    Réglages de TabMyAccount

--- app/src/main/java/foodget/ihm/foodget/activities/NewPasswordActivity.java
+++ app/src/main/java/foodget/ihm/foodget/activities/NewPasswordActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import foodget.ihm.foodget.DatabaseHelper;
 import foodget.ihm.foodget.R;
